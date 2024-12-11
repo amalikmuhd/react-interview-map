950e7fca27d01ac248c5925532c9c8945c83c611
Abdulmalik Muhammad <amalikmuhdd@gmail.com>
2024-12-11 20:21:32 +0100

    feat: done the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 // Create a simple React application that displays a list of countries and their capitals
 // The application should have the following features:
 
+import { useEffect, useState } from "react";
+
 // The list of countries and capitals should be fetched from an API
 // Each country should be displayed in a separate component
+
 // The user should be able to filter the list by capital
 
 /**
@@ -14,14 +17,8 @@ const BASE_URL = "https://restcountries.com/v3.1";
   To filter by capital city, use the '/capital/{capital}' endpoint
  */
 
-const FILTERABLE_CAPITALS = [
-  "Tallinn",
-  "Helsinki",
-  "Stockholm",
-  "Oslo",
-  "Copenhagen",
-  "Reykjavik",
-] as const;
+const FILTERABLE_CAPITALS = ["Tallinn", "Helsinki", "Stockholm", "Oslo", "Copenhagen", "Reykjavik"] as const;
+
 type Capital = (typeof FILTERABLE_CAPITALS)[number];
 
 interface Country {
@@ -31,8 +28,59 @@ interface Country {
   capital: string;
 }
 
-export default function App() {
+const Row = ({ name, capital }: Country) => {
   return (
-    <div className="App"><h1>React Interview</h1></div>
+    <div>
+      <p>
+        {name.common} ,{capital}
+      </p>
+    </div>
+  );
+};
+
+export default function App() {
+  const [data, setData] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState<Capital>();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${BASE_URL}${value ? `/capital/${value}` : "/all"}`);
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [value]);
+
+  return (
+    <div className="App">
+      <h1>React Interview</h1>
+      <select onChange={handleChange}>
+        {FILTERABLE_CAPITALS.map((item, index) => (
+          <option key={index} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+      {/* <input placeholder="Search me" value={value} onChange={(e) => setValue(e.target.value as never)} /> */}
+      {loading ? (
+        <p>loading....</p>
+      ) : (
+        data.map((item, index) => <Row key={index} name={item.name as never} capital={item?.capital} />)
+      )}
+    </div>
   );
 }
